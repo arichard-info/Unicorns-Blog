4a66513d84e29248050155d371c7b32fd827956c
Unknown <antoiner.info@gmail.com>
2018-04-27 20:50:29 +0200

    Add tests

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     release_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField(verbose_name='Content')
+    image = models.ImageField(upload_to='image', verbose_name='Image')
     def  __str__(self):
         return self.title
     class Meta:
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.db.models.query import QuerySet
+from blog.models import Article
 
 # Create your tests here.
+
+class WebsiteTestCase(TestCase):
+    fixtures = ['initial.json', ]
+    def test_index_page(self):
+        response = self.client.get(reverse('blog:index'))
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertTemplateUsed('index.html')
+
+        
+    def test_post_page(self):
+        # Published news
+        article = Article.objects.first()
+        response = self.client.get(reverse('blog:article', kwargs={'slug': article.slug}))
+        self.assertContains(response, article.title)
+        self.assertEqual(type(response.context['article']), Article)
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertTemplateUsed('article.html')
