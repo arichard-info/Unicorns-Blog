c18cc55faf2803a9a163120c11829043073ec9b0
Unknown <antoiner.info@gmail.com>
2018-04-27 13:13:33 +0200

    Amélioration structure templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('article/<slug:slug>', views.article, name='article'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Article, Comment
+from .forms import CommentForm
 
 # Create your views here.
 def index(request):
     articles = Article.objects.all()[:3]
-    return render(request, 'index.html', {'latests_articles': articles})
+    return render(request, 'partials/index.html', {'latests_articles': articles})
 
 def article(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, 'article.html', {'article': article})
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article = article
+            comment.save()
+            return redirect('/article/' + slug)
+    else:
+        form = CommentForm()
+    return render(request, 'partials/single_article.html', {'article': article, 'form' : form})
+
+
+def about(request):
+    return render(request, 'partials/about.html', {})
+
+def contact(request):
+    return render(request, 'partials/contact.html', {})
