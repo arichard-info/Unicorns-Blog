bccc33e97e3ef353196fcbf00e3ffd0fae9c3a03
Unknown <antoiner.info@gmail.com>
2018-04-28 18:12:21 +0200

    ajout styles header desktop

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1,3 +1,12 @@
 $(function() {
-    
+    $("#search-field").on("focus", function(event){
+    	$(event.currentTarget).parent().addClass("active");
+    })
+
+     $(document).click(function(event) { 
+	    if(!$(event.target).closest('#search-form').length) {
+	      
+	            $('#search-form').removeClass("active");
+	    }        
+	});
 });
