62c1d910c6c30a4653af98cf3805b2a0b670f55c
Unknown <antoiner.info@gmail.com>
2018-04-30 17:17:48 +0200

    ajout slug automatique

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone  
+from django.utils.text import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=140, unique=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     release_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField(verbose_name='Content')
     image = models.ImageField(upload_to='image', verbose_name='Image')
     def  __str__(self):
         return self.title
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Article.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save()
     class Meta:
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
