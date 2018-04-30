4b1c437f31f6b4cc488793aaf39d6e987435d29e
Unknown <antoiner.info@gmail.com>
2018-04-30 13:02:37 +0200

    ajout de quelques tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.db.models.query import QuerySet
-from blog.models import Article
+from blog.models import Article, Comment
 
 # Create your tests here.
 
@@ -9,15 +9,32 @@ class WebsiteTestCase(TestCase):
     fixtures = ['initial.json', ]
     def test_index_page(self):
         response = self.client.get(reverse('blog:index'))
+        articles = [article.__repr__() for article in Article.objects.all().order_by('-release_date')[:6]]
+        self.assertQuerysetEqual(response.context['articles'], articles)
+        self.assertTemplateUsed('partials/index.html')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertTemplateUsed('index.html')
+        
 
         
-    def test_post_page(self):
-        # Published news
+    def test_article_page(self):
         article = Article.objects.first()
         response = self.client.get(reverse('blog:article', kwargs={'slug': article.slug}))
         self.assertContains(response, article.title)
         self.assertEqual(type(response.context['article']), Article)
+        self.assertTemplateUsed('partials/single_article.html')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertTemplateUsed('article.html')
+        
+
+
+    def test_about_page(self):
+        response = self.client.get(reverse('blog:about'))
+        self.assertTemplateUsed('partials/about.html')
+        self.failUnlessEqual(response.status_code, 200)
+        
+
+
+    def test_search_page(self):
+        response = self.client.get(reverse('blog:search'))
+        self.assertTemplateUsed('partials/search.html')
+        self.failUnlessEqual(response.status_code, 200)
+        
